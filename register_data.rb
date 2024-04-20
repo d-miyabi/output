43750de5d866f93c89687f9ca3c206201a8b7d32
d-miyabi <msgh3110+works@gmail.com>
2024-04-20 11:22:12 +0900

    No.55 register_data

--- register_data.rb
+++ register_data.rb
@@ -1,11 +1,33 @@
-def register_data
-  puts "データの登録"
+def register_data(data)
+  puts "名前を入力してください"
+  name = gets.chomp
+  puts "年齢を入力してください"
+  age = gets.chomp
+  puts "身長を入力してください(m単位)"
+  tall = gets.to_f
+  puts "体重を入力してください"
+  weight = gets.to_f
+  person = { name: name, age: age, tall: tall, weight: weight }
+  data << person
+ end
+
+def show_data_list(data)
+  puts "見たい人の番号を選択してください"
+  data.each_with_index do |person, index|
+    puts "#{index + 1}: #{person[:name]}"
+  end
+  input = gets.to_i - 1
+  show_data(data[input])
 end
 
-def show_data
-  puts "データの確認"
+def show_data(data)
+  puts "名前:#{data[:name]}"
+  puts "年齢:#{data[:age]}"
+  puts "身長:#{data[:tall]}"
+  puts "体重:#{data[:weight]}"
 end
 
+data = []
 while true
   puts "選択してください"
   puts "[0]登録する"
@@ -14,9 +36,9 @@ while true
   input = gets.to_i
 
   if input == 0
-    register_data
+    register_data(data)
   elsif input == 1
-    show_data
+    show_data_list(data)
   elsif input == 2
     exit
   else
